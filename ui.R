6358895aa5f567cfecd4cee9c0b386e304c5fd5d
whisperxxxx2 <110135477+whisperxxxx2@users.noreply.github.com>
2022-10-02 12:46:37 +0100

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,1 +1,17 @@
+library(datasets)
+library(shiny)
+
+shinyUI(fluidPage(
+  titlePanel("Telephones by region"),
+  sidebarLayout(      
+    sidebarPanel(
+      selectInput("region", "Region:", 
+                  choices=colnames(WorldPhones))
+    ),
+    mainPanel(
+      plotOutput("Plot1")  
+    )
+    
+  )
+))
 
