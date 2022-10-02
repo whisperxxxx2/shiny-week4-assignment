780dabbb01911e76d1243b1e8dd4350cddbd8084
whisperxxxx2 <110135477+whisperxxxx2@users.noreply.github.com>
2022-10-02 12:46:51 +0100

    Update server.R

--- server.R
+++ server.R
@@ -1,1 +1,15 @@
+library(datasets)
+library(shiny)
+
+shinyServer(function(input, output) {
+
+  output$Plot1 <- renderPlot({
+
+    barplot(WorldPhones[,input$region]/1000, 
+            main=input$region,
+            ylab="Number of Telephones(million)",
+            xlab="Year")
+    })
+
+})
 
